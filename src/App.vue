1d186fb3f3b7bafcaaec3f24e8993ef09e4d2e4a
abang <839364887@qq.com>
2019-06-07 23:17:10 +0800

    '第一次提交'

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>index</p>
+      <el-button type="success">成功按钮</el-button>
+      <p>
+        <router-view></router-view>
+      </p>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import axios from 'axios';
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+//   name: 'app',
+//   components: {
+//     HelloWorld
+//   }
+
+  mounted(){
+    //请求测试接口
+    // const res = axios({
+    //   url:'https://api.github.com/',
+    //   method:'GET',
+    //   //get请求参数
+    //   params:{
+    //     abc:123
+    //   }
+    // }).then(res => {
+    //   console.log(res);
+    // })
+    
+    this.$axios({
+       url:'https://api.github.com/',
+      method:'GET',
+    }).then(res => {
+      console.log(res);
+    })
+    
+
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
+//导入
+import axios from 'axios'
+//导入路由
+import VueRouter from 'vue-router'
+
+//导入饿了么组件
+import ElementUI from "element-ui"
+import Login from "./pages/Login.vue"
+
+//element的样式
+import 'element-ui/lib/theme-chalk/index.css';
+
+//注册饿了么组件
+Vue.use(ElementUI)
+// 注册路由
+Vue.use(VueRouter)
+// 配置路由
+const routes = [
+  {path:'/login',component:Login}
+];
+
+const router = new VueRouter({ routes })
 
 Vue.config.productionTip = false
+//把axios绑定到原型 实现优化
+Vue.prototype.$axios = axios
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
